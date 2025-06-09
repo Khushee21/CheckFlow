fb5ca5f4f8b669baf374fd2a13f6dc111eb1a040
Khushee21 <khushirathore649@gmail.com>
2025-06-09 21:53:29 +0530

    done

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,29 +4,20 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import AddTaskRouter from "./router/AddTaskRouter";
 
-dotenv.config();
+dotenv.config({
+  path:"./env"
+});
 
 const app = express();
 
-const allowedOrigins = [
-  'https://checkflow-client-s1fd.vercel.app', 
-  'http://localhost:3000'                    
-];
-
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,  
-}));
-
+  origin:"https://checkflow-client-s1fd.vercel.app" ,
+  methods:['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders:['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  exposedHeaders:['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+  credentials:true,
+  preflightContinue:false,
+}))
 app.use(express.json());
 
 app.use('/task', AddTaskRouter);
